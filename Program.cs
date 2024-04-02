5fd60dc140a75c1e4cd9c6bce6767997f4eba423
Sergey Perekraov <sperekraov@mail.ru>
2024-04-02 13:55:24 +0500

    Решена первая задача

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+
+
+static void NaturalWrite(int m, int n)
+{
+    m+=1;
+    if (m < n )
+    {
+        Console.WriteLine(m);
+        NaturalWrite (m, n);
+    }
+}
+
+Console.Write("Input Low limit: ");
+int m = System.Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input Upper limit: ");
+int n = System.Convert.ToInt32(Console.ReadLine());
+
+NaturalWrite(m, n);
